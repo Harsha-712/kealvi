36ccbefb9ba0546bd63a92653a3ab7c89eea43d4
harsh7122006 <harshavardhinisi843@gmail.com>
2026-06-03 15:54:00 +0530

    changed page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-export default function Page() {
+import { getQuestionsPage } from "@/lib/questions";
+
+export default async function Page() {
+  const { questions } = await getQuestionsPage(0, 10);
+
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1>Questions Loaded</h1>
+      <p>{questions.length}</p>
     </main>
   );
 }
